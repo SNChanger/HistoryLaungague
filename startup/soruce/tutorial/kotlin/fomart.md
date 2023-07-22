ccb0b737234d239f4dca15d0211063a220ddac45
SNChanger <skuazt81@hotmail.co.jp>
2023-07-22 19:54:37 +0900

    println系
    　・文字フォーマット関連

---
+++ startup/soruce/tutorial/kotlin/fomart.md
@@ -0,0 +1,10 @@
+# 概要
+
+フォーマット関数での利用パターンをまとめた、プログラムの説明になります。
+
+# 出力結果
+
+add value 121
+sub value 118
+mul value 360
+division value 60
---
+++ startup/soruce/tutorial/kotlin/format.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    
+    // 計算の基本値として利用する
+    var baseValue = 120;
+    
+    // 文字フォーマット内加算の例
+    println("add value ${baseValue + 1}")
+
+    // 文字フォーマット内減算の例
+    println("sub value ${baseValue - 2 }")
+
+    // 文字フォーマット内積の例
+    println("mul value ${baseValue * 3}")
+
+    // 文字フォーマット内割り算の例
+    println("division value ${baseValue / 2}")
+
+    
+}
